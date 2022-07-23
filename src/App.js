14be185ad309f186dccc38770b0ae0d83571ced9
Divyansh Singh <divyansh44066@gmail.com>
2022-07-23 19:59:22 +0530

    all file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Format from './Format'
 
 function App() {
+  const [data, setData] = useState()
+  const [jsonData, setJsonData] = useState()
+  const [error, setError] = useState(false)
+  const [parentCollapse, setParentCollapse] = useState()
+
+
+  function isValidJSONString(str) {
+    try {
+      JSON.parse(str);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
+  function callbackFunction() {
+
+    if (isValidJSONString(data)) {
+       let s = data.replaceAll('.', '')
+       console.log(s)
+      setJsonData(JSON.parse(data))
+      console.log(Object.keys(JSON.parse(data)).length)
+      let arr = []
+      for (var i = 0; i <= Object.keys(JSON.parse(data)).length; i++) {
+        arr.push({
+          key: i,
+          watch: false,
+        });
+      }
+      setParentCollapse(arr)
+      setError(false)
+    } else {
+      setError(true)
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='flex items-center justify-center mr-10 mb-2 mt-2'>
+
+        <button className='bg-red-500 pl-8 pr-8 pt-2
+   pb-2 rounded-md font-semibold text-white' onClick={(e) => callbackFunction(e)}>Format</button>
+        <button className='bg-green-500 pl-8 pr-8 pt-2
+   pb-2 rounded-md font-semibold text-white ml-10' onClick={() => {
+            setJsonData('')
+            setData('')
+            setError('')
+          }}>Clear </button>
+      </div>
+      <div className='flex'>
+        <textarea type='text' className='bg-gray-200 w-[50vw] h-[90vh] resize-none border-2 border-gray-300 outline-none' placeholder='ENTER YOUR VALID JSON DATA' value={data} onChange={(e) => setData(e.target.value)} />
+        <div className='h-[90vh] overflow-auto w-[50vw] '>
+
+          <Format datas={jsonData} error={error} parentCollapse={parentCollapse} setParentCollapse={setParentCollapse} />
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Format.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react'
+import Double from './images/double.png'
+
+
+
+const Menu = ({ data }) => {
+    return (
+        <>
+            {
+                Object.entries(data).map(([item, i], ind) => (
+                    <>
+
+                        <h1 className='text-blacks font-semibold mb-1'>
+
+                            {JSON.stringify(item)}:
+                            {typeof i === 'number' ?
+                                <span className='text-red-500 font-semibold'> {JSON.stringify(i)}  </span>
+                                : typeof i === 'object' ?
+
+                                    <span >&#123;
+                                        <Menu data={i} />
+                                        &#125;
+                                    </span>
+                                    :
+                                    <span className='text-green-600 font-semibold'> {JSON.stringify(i)}  </span>
+                            }
+                        </h1>
+
+                    </>
+                ))
+            }
+
+        </>
+
+    );
+}
+export default function Format({ datas, error, parentCollapse, setParentCollapse }) {
+
+
+
+
+    const setToLocal = (value) => {
+        const data = parentCollapse.map((feed) => ({
+            ...feed,
+            watch: feed.key == value ? true : feed.watch
+        }))
+        setParentCollapse(data)
+    }
+    const remove = (value) => {
+        const data = parentCollapse.map((feed) => ({
+            ...feed,
+            watch: feed.key == value ? false : feed.watch
+        }))
+        setParentCollapse(data)
+
+    }
+
+    return (
+        <div className='ml-10 mt-4'>
+            {datas ?
+                <div>&#123;
+                </div>
+                :
+                ' Your formated JSON output'
+            }
+            {
+                error && <p className='text-red-500 font-semibold'>Syntax error: please enter a valid json formate data</p>
+            }
+            {
+                Object.entries(datas ? datas : '').map(([item, i], ind) => (
+                    <>
+
+                        <h1 className='text-blacks font-semibold mb-1'>
+                            {
+                                parentCollapse && parentCollapse[ind].watch == true &&
+                                <p className='inline mr-6 bg-pink-300 pl-2 pr-2 cursor-pointer' onClick={() => remove(ind)}>-</p>
+                            }
+                            {JSON.stringify(item)}:
+
+
+                            {typeof i === 'number' ?
+                                <span className='text-red-500'> {JSON.stringify(i)} : </span>
+                                : typeof i === 'object' ?
+                                    parentCollapse && parentCollapse[ind].watch == true ?
+                                        <span>[
+
+                                            <Menu data={i} />
+                                            ]
+                                        </span>
+                                        : <span>[
+                                            <img src={Double} className='inline cursor-pointer' onClick={() => setToLocal(ind)} />
+                                            ]
+                                        </span>
+                                    :
+                                    <span className='text-green-600 font-semibold'> {JSON.stringify(i)} </span>
+
+                            }
+                        </h1>
+
+                    </>
+                ))
+            }
+            {datas &&
+                <div>&#125;</div>
+            }
+        </div>
+    )
+}
+
