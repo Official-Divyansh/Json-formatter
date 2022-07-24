e9ba46eee33fffacbb9912b968296c2609c764bb
Official-Divyansh <107239607+Official-Divyansh@users.noreply.github.com>
2022-07-24 14:57:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
 
   return (
     <>
-      <div className='flex items-center justify-center mr-10 mb-2 mt-2'>
+      <div className='flex  items-center justify-center mr-10 mb-2 mt-2'>
 
         <button className='bg-red-500 pl-8 pr-8 pt-2
    pb-2 rounded-md font-semibold text-white' onClick={(e) => callbackFunction(e)}>Format</button>
@@ -55,9 +55,9 @@ function App() {
             setError('')
           }}>Clear </button>
       </div>
-      <div className='flex'>
-        <textarea type='text' className='bg-gray-200 w-[50vw] h-[90vh] resize-none border-2 border-gray-300 outline-none' placeholder='ENTER YOUR VALID JSON DATA' value={data} onChange={(e) => setData(e.target.value)} />
-        <div className='h-[90vh] overflow-auto w-[50vw] '>
+      <div className='flex flex-col sm:flex-row'>
+        <textarea type='text' className='bg-gray-200 w-screen sm:w-[50vw] h-[45vh] sm:h-[90vh] resize-none border-2 border-gray-300 outline-none' placeholder='ENTER YOUR VALID JSON DATA' value={data} onChange={(e) => setData(e.target.value)} />
+        <div className='h-[45vh] sm:h-[90vh] overflow-auto w-screen sm:w-[50vw] '>
 
           <Format datas={jsonData} error={error} parentCollapse={parentCollapse} setParentCollapse={setParentCollapse} />
         </div>
